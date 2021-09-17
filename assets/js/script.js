725006d82e04492f74810a21715bc620338fba56
McTastic <mctastic2121@gmail.com>
2021-09-17 14:56:45 -0400

    changed search box to dropdown and changed header to only have 2 links

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,10 @@ $(document).ready(function() {
     $(window).on('scroll', function() {
      if($(window).scrollTop() < 1000) {
        $('.hero').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
-       $('.hero h1',).css('top', 35 + ($(window).scrollTop() * .1) + '%');
+       $('.hero h1',).css('top', 30 + ($(window).scrollTop() * .1) + '%');
        $('.hero h1',).css('opacity', 1 - ($(window).scrollTop() * .003));
-       $('.nav-wrapper',).css('top', 50 + ($(window).scrollTop() * .1) + '%');
-       $('.nav-wrapper',).css('opacity', 1 - ($(window).scrollTop() * .003));
-       $('.nav-search',).css('top', 0 + ($(window).scrollTop() * .1) + '%');
-       $('.nav-search',).css('opacity', 0 + ($(window).scrollTop() * .003));
+       $('.dropdown-trigger',).css('top', 50 + ($(window).scrollTop() * .1) + '%');
+       $('.dropdown-trigger',).css('opacity', 1 - ($(window).scrollTop() * .003));
      }
       
       if($(window).scrollTop() >= $('.content-wrapper').offset().top - 400) {
