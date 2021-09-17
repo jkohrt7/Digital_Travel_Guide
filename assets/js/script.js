6356548ec72d9900dfbadedb16f537831b076f77
McTastic <mctastic2121@gmail.com>
2021-09-16 20:49:35 -0400

    added search box to hero image and to nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,10 @@ $(document).ready(function() {
        $('.hero').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
        $('.hero h1',).css('top', 50 + ($(window).scrollTop() * .1) + '%');
        $('.hero h1',).css('opacity', 1 - ($(window).scrollTop() * .003));
-       $('.hero p',).css('top', 60 + ($(window).scrollTop() * .1) + '%');
-       $('.hero p',).css('opacity', 1 - ($(window).scrollTop() * .003));
+       $('.nav-wrapper',).css('top', 61 + ($(window).scrollTop() * .1) + '%');
+       $('.nav-wrapper',).css('opacity', 1 - ($(window).scrollTop() * .003));
+       $('.nav-search',).css('top', 0 + ($(window).scrollTop() * .1) + '%');
+       $('.nav-search',).css('opacity', 0 + ($(window).scrollTop() * .003));
      }
       
       if($(window).scrollTop() >= $('.content-wrapper').offset().top - 400) {
